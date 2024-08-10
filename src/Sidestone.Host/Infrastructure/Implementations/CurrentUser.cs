e0f4deabae80d30d78dddfc5a67d9d926a76b9d3
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-08-10 15:58:47 +0100

    add currentUser service

---
+++ src/Sidestone.Host/Infrastructure/Implementations/CurrentUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Sidestone.Host.Infrastructure.Interfaces;
+
+namespace Sidestone.Host.Infrastructure.Implementations
+{
+    public class CurrentUser(IHttpContextAccessor httpContextAccessor) : ICurrentUser
+    {
+        public string? UserId =>
+            // Retrieve the user ID from the JWT token
+            httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public string? Email =>
+            // Retrieve the email from the JWT token
+            httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
+
+        public string? Role =>
+            // Retrieve the role from the JWT token
+            httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Role)?.Value;
+    }
+}
---
+++ src/Sidestone.Host/Infrastructure/Interfaces/ICurrentUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Sidestone.Host.Infrastructure.Interfaces
+{
+    public interface ICurrentUser
+    {
+        string? UserId { get; }
+        string? Email { get; }
+        string? Role { get; }
+    }
+}
